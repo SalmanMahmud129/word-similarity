7b19ea032cac4127a897ae1460609309a7c704c9
Salman Mahmud <salman_mahmud@hotmail.com>
2023-03-27 19:36:21 -0400

    sign in button

---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { FC, useState } from 'react'
+import Button from './ui/Button'
+import { signIn } from 'next-auth/react'
+import { toNamespacedPath } from 'path'
+
+interface SignInButtonProps {
+  
+}
+
+const SignInButton: FC<SignInButtonProps> = ({}) => {
+    const [isLoading, setIsLoading] = useState(false)
+
+    const signInWithGoogle = async () =>{
+        setIsLoading(true)
+
+        try{
+            await signIn('google')
+        } catch (error){
+            toast({
+                title: 'Error signing in',
+                message: 'Please try again later',
+                type: 'error'
+            })
+        }
+    }
+
+
+  return <Button onClick={signInWithGoogle} isLoading={isLoading}>
+
+  </Button>
+}
+
+export default SignInButton
